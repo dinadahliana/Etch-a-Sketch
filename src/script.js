ac7cc46ed02d022d2ac464aeb9f3508755543f10
dinadahliana <dina.dahliana@students.itb.ac.id>
2023-03-06 10:50:36 +0700

    setting up color mode

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+const DEFAULT_SIZE = 16;
+let colorMode = "default";
+
 // function to build grid
 const container = document.getElementById("canvas");
 
@@ -11,8 +14,6 @@ function makeGrid(x) {
     };
   };
 
-makeGrid(16);
-
 // display value and grid based of the user input
 const value = document.querySelector("#value");
 const input = document.querySelector("#pi_input");
@@ -24,18 +25,57 @@ input.addEventListener("input", (event) => {
   value.textContent = `${event.target.value} x ${event.target.value}`;
 
   while (container.firstChild) {
-    container.removeChild(container.lastChild);
-};
+    container.removeChild(container.lastChild)
+  };
 
   makeGrid(input.value);
 
 });
 
-// setting up hovering 
 
+// setting up color mode
+const randomColorBtn = document.querySelector(".color");
+const eraserBtn = document.querySelector(".eraser");
+const defaultColorBtn = document.querySelector(".default");
+
+randomColorBtn.addEventListener("click", () => {
+    colorMode = "color";
+    console.log("color button was clicked");
+});
+
+eraserBtn.addEventListener("click", () => {
+    colorMode = "eraser";
+    console.log("erase button was clicked");
+});
+
+defaultColorBtn.addEventListener("click", () => {
+    colorMode = "default";
+    console.log("default color button was clicked");
+});
+
+// setting up hovering 
 function changeColor(e) {
-    e.target.style.backgroundColor = "black";
-}
+    if (colorMode === "color") {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+
+    if (colorMode === "default") {
+        e.target.style.backgroundColor = "#252323";
+    }
+
+    if (colorMode === "eraser") {
+        e.target.style.backgroundColor = "#f5f1ed";
+    }
+};
+
+
+window.onload = () => {
+    makeGrid(DEFAULT_SIZE);
+};
+
 
 
 
